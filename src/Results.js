542af1795091b1f1a74e31441a6cd92cb2b08d1e
KV Nguyen <kieuvyn12@gmail.com>
2019-06-15 19:11:49 -0400

    rendering articles in Results component 5 at a time

--- src/Results.js
+++ src/Results.js
@@ -1,8 +1,85 @@
 import React from 'react'
 
 class Results extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      allArticles: this.props.location.state.articles,
+      top5: this.props.location.state.articles.slice(0, 5),
+      top10: [],
+      top15: [],
+      top20: []
+    }
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onSubmit() {
+    if (this.state.top10.length === 0) {
+      this.setState({
+        top10: this.props.location.state.articles.slice(5, 10)
+      })
+    } else if (this.state.top15.length === 0) {
+      this.setState({
+        top15: this.props.location.state.articles.slice(10, 15)
+      })
+    } else if (this.state.top20.length === 0) {
+      this.setState({
+        top20: this.props.location.state.articles.slice(15, 20)
+      })
+    }
+  }
+
   render() {
-    return <h1>this is the results component</h1>
+    return (
+      <div>
+        <h1>breaking news articles:</h1>
+        {this.state.allArticles.length !== 0 ? (
+          <div>
+            {this.state.top5.map(article => (
+              <h3 key={this.state.top5.indexOf(article)}>{article.title}</h3>
+            ))}
+          </div>
+        ) : (
+          <br />
+        )}
+
+        {this.state.top5.length !== 0 ? (
+          <div>
+            {this.state.top10.map(article => (
+              <h3 key={this.state.top10.indexOf(article)}>{article.title}</h3>
+            ))}
+          </div>
+        ) : (
+          <br />
+        )}
+
+        {this.state.top10.length !== 0 ? (
+          <div>
+            {this.state.top15.map(article => (
+              <h3 key={this.state.top15.indexOf(article)}>{article.title}</h3>
+            ))}
+          </div>
+        ) : (
+          <br />
+        )}
+
+        {this.state.top15.length !== 0 ? (
+          <div>
+            {this.state.top20.map(article => (
+              <h3 key={this.state.top20.indexOf(article)}>{article.title}</h3>
+            ))}
+          </div>
+        ) : (
+          <br />
+        )}
+
+        {this.state.top20.length === 0 ? (
+          <button onClick={this.onSubmit}>click to get 5 more!</button>
+        ) : (
+          <br />
+        )}
+      </div>
+    )
   }
 }
 
