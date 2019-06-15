5931b97597a0a949d9f8af42c97014e071c319c0
KV Nguyen <kieuvyn12@gmail.com>
2019-06-15 18:49:49 -0400

    API call is routed up and populating Home component's state with top 20 articles

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,34 @@
 import React from 'react'
 
 class Home extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      articles: []
+    }
+    this.getArticles = this.getArticles.bind(this)
+  }
+
+  componentDidMount() {
+    this.getArticles()
+  }
+
+  async getArticles() {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${
+      process.env.REACT_APP_API_KEY
+    }`
+    let response = await fetch(url)
+    let data = await response.json()
+    this.setState({
+      articles: data.articles
+    })
+  }
+
   render() {
     return (
       <div>
-        <h1>home component</h1>
+        {console.log(this.state)}
+        <h1>Click below to get a list of the top 5 breaking news in the US!</h1>
       </div>
     )
   }
