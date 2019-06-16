665a3b5eb51a59fbc109c324672a30d3e8861a43
KV Nguyen <kieuvyn12@gmail.com>
2019-06-16 10:32:05 -0400

    created containers for Article and Results components for styling

--- src/Article.js
+++ src/Article.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {Jumbotron} from 'react-bootstrap'
+import {Jumbotron, Container} from 'react-bootstrap'
 
 class Article extends React.Component {
   render() {
@@ -11,19 +11,25 @@ class Article extends React.Component {
           <br />
           <p>{this.props.location.state.article.description}</p>
         </Jumbotron>
-        <img
-          src={this.props.location.state.article.urlToImage}
-          alt="pic from news source"
-        />
-        <h5>Author: {this.props.location.state.article.author}</h5>
-        <h6>
-          Published at: {this.props.location.state.article.publishedAt} from:{' '}
-          {this.props.location.state.article.source.name}
-        </h6>
-        <p>Preview of article:{this.props.location.state.article.content}</p>
-        <a href={this.props.location.state.article.url}>
-          click here to see from original source
-        </a>
+        <Container>
+          <div>
+            <img
+              src={this.props.location.state.article.urlToImage}
+              alt="pic from news source"
+            />
+            <h5>Author: {this.props.location.state.article.author}</h5>
+            <h6>
+              Published at: {this.props.location.state.article.publishedAt}{' '}
+              from: {this.props.location.state.article.source.name}
+            </h6>
+            <p>
+              Preview of article:{this.props.location.state.article.content}
+            </p>
+            <a href={this.props.location.state.article.url}>
+              click here to see from original source
+            </a>
+          </div>
+        </Container>
       </div>
     )
   }
--- src/Results.js
+++ src/Results.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import FilterForm from './FilterForm'
 
-import {Jumbotron} from 'react-bootstrap'
+import {Jumbotron, Container, Col} from 'react-bootstrap'
 
 class Results extends React.Component {
   constructor(props) {
@@ -54,99 +54,103 @@ class Results extends React.Component {
           <br />
           <h5>Delivering you the latest top news headlines in the US:</h5>
         </Jumbotron>
-        <FilterForm
-          articles={this.state.allArticles}
-          filtered={this.filtered}
-        />
-        {this.state.allArticles.length !== 0 ? (
-          <div>
-            {this.state.top5.map(article => (
-              <Link
-                to={{
-                  pathname: '/article',
-                  state: {
-                    article: article
-                  }
-                }}
-              >
-                <h3 key={this.state.allArticles.indexOf(article)}>
-                  {article.title}
-                </h3>
-              </Link>
-            ))}
-          </div>
-        ) : (
-          <br />
-        )}
+        <Container>
+          <FilterForm
+            articles={this.state.allArticles}
+            filtered={this.filtered}
+          />
+        </Container>
+        <Container>
+          {this.state.allArticles.length !== 0 ? (
+            <div>
+              {this.state.top5.map(article => (
+                <Link
+                  to={{
+                    pathname: '/article',
+                    state: {
+                      article: article
+                    }
+                  }}
+                >
+                  <h3 key={this.state.allArticles.indexOf(article)}>
+                    {article.title}
+                  </h3>
+                </Link>
+              ))}
+            </div>
+          ) : (
+            <br />
+          )}
 
-        {this.state.top5.length !== 0 ? (
-          <div>
-            {this.state.top10.map(article => (
-              <Link
-                to={{
-                  pathname: '/article',
-                  state: {
-                    article: article
-                  }
-                }}
-              >
-                <h3 key={this.state.allArticles.indexOf(article)}>
-                  {article.title}
-                </h3>
-              </Link>
-            ))}
-          </div>
-        ) : (
-          <br />
-        )}
+          {this.state.top5.length !== 0 ? (
+            <div>
+              {this.state.top10.map(article => (
+                <Link
+                  to={{
+                    pathname: '/article',
+                    state: {
+                      article: article
+                    }
+                  }}
+                >
+                  <h3 key={this.state.allArticles.indexOf(article)}>
+                    {article.title}
+                  </h3>
+                </Link>
+              ))}
+            </div>
+          ) : (
+            <br />
+          )}
 
-        {this.state.top10.length !== 0 ? (
-          <div>
-            {this.state.top15.map(article => (
-              <Link
-                to={{
-                  pathname: '/article',
-                  state: {
-                    article: article
-                  }
-                }}
-              >
-                <h3 key={this.state.allArticles.indexOf(article)}>
-                  {article.title}
-                </h3>
-              </Link>
-            ))}
-          </div>
-        ) : (
-          <br />
-        )}
+          {this.state.top10.length !== 0 ? (
+            <div>
+              {this.state.top15.map(article => (
+                <Link
+                  to={{
+                    pathname: '/article',
+                    state: {
+                      article: article
+                    }
+                  }}
+                >
+                  <h3 key={this.state.allArticles.indexOf(article)}>
+                    {article.title}
+                  </h3>
+                </Link>
+              ))}
+            </div>
+          ) : (
+            <br />
+          )}
 
-        {this.state.top15.length !== 0 ? (
-          <div>
-            {this.state.top20.map(article => (
-              <Link
-                to={{
-                  pathname: '/article',
-                  state: {
-                    article: article
-                  }
-                }}
-              >
-                <h3 key={this.state.allArticles.indexOf(article)}>
-                  {article.title}
-                </h3>
-              </Link>
-            ))}
-          </div>
-        ) : (
-          <br />
-        )}
+          {this.state.top15.length !== 0 ? (
+            <div>
+              {this.state.top20.map(article => (
+                <Link
+                  to={{
+                    pathname: '/article',
+                    state: {
+                      article: article
+                    }
+                  }}
+                >
+                  <h3 key={this.state.allArticles.indexOf(article)}>
+                    {article.title}
+                  </h3>
+                </Link>
+              ))}
+            </div>
+          ) : (
+            <br />
+          )}
 
-        {this.state.top20.length === 0 ? (
-          <button onClick={this.onSubmit}>See 5 more articles!</button>
-        ) : (
-          <br />
-        )}
+          {this.state.top20.length === 0 ? (
+            <button onClick={this.onSubmit}>See 5 more articles!</button>
+          ) : (
+            <br />
+          )}
+        </Container>
       </div>
     )
   }
