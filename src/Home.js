e816778861bac2c14ff9dedbffac12c0303a9c6f
KV Nguyen <kieuvyn12@gmail.com>
2019-06-16 11:40:32 -0400

    added carousel styling to homepage and slight style.css tweaks

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {Jumbotron, Button} from 'react-bootstrap'
+import {Jumbotron, Button, Carousel, Container, Col} from 'react-bootstrap'
 
 class Home extends React.Component {
   constructor() {
@@ -36,27 +36,79 @@ class Home extends React.Component {
 
   render() {
     return (
-      <Jumbotron>
-        <h1>Welcome to the Brace News app!</h1>
-        <p>
-          We aggregate live top and breaking headlines for the United States
-          from multiple news sources.
-        </p>
-        <p>
-          <Link
-            to={{
-              pathname: '/results',
-              state: {
-                articles: this.state.articles
-              }
-            }}
-          >
-            <Button variant="primary" onClick={this.onSubmit}>
-              See news
-            </Button>{' '}
-          </Link>
-        </p>
-      </Jumbotron>
+      <div>
+        <Jumbotron>
+          <h1>Welcome to the Brace News app!</h1>
+          <p>
+            We aggregate live top and breaking headlines for the United States
+            from multiple news sources.
+          </p>
+          <p>
+            <Link
+              to={{
+                pathname: '/results',
+                state: {
+                  articles: this.state.articles
+                }
+              }}
+            >
+              <Button variant="primary" onClick={this.onSubmit}>
+                See news
+              </Button>{' '}
+            </Link>
+          </p>
+        </Jumbotron>
+        <Container>
+          <Col sm="11">
+            <Carousel>
+              <Carousel.Item className="carousel-item">
+                <img
+                  className="d-block w-100"
+                  src="https://images.unsplash.com/photo-1498644035638-2c3357894b10?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
+                  alt="unsplash pic"
+                />
+                <Carousel.Caption className="carousel-text">
+                  <h3>Why Brace News?</h3>
+                  <p>
+                    We are an excellent way to follow the latest in breaking
+                    news.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className="carousel-item">
+                <img
+                  className="d-block w-100"
+                  src="https://images.unsplash.com/photo-1507581332893-aefc5acf08e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
+                  alt="unsplash pic"
+                />
+
+                <Carousel.Caption className="carousel-text">
+                  <h3>Where do you get your sources?</h3>
+                  <p>
+                    We aggreggate articles from over 30,000 news sources and
+                    blogs.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className="carousel-item">
+                <img
+                  className="d-block w-100"
+                  src="https://images.unsplash.com/photo-1560177112-fbfd5fde9566?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80"
+                  alt="unsplash pic"
+                />
+
+                <Carousel.Caption className="carousel-text">
+                  <h3>Looking for something in particular?</h3>
+                  <p>
+                    Use our filter function to find exactly what you're searchin
+                    for.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </Col>
+        </Container>
+      </div>
     )
   }
 }
